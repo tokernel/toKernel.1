16f9be02195ad33c3a10c6cbd0a75ecd5bf9b34f
Development team <tokernel@gmail.com>
2016-12-17 16:21:17 +0400

    Version 1.6.0

--- tokernel.framework/config/constants.php
+++ tokernel.framework/config/constants.php
@@ -42,6 +42,7 @@ define('TK_SHORT_NAME', 'toKernel');
 define('TK_DESCRIPTION', 'Universal PHP Framework');
 
 /* Project version */
+define('TK_VERSION', '1.6.0');
 
 /* Short name for DIRECTORY_SEPARATOR */
 define('TK_DS', DIRECTORY_SEPARATOR);
